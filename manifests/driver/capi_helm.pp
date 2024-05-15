02de6972f749a1f84776b37b7e297d8ae2f4628a
Jake Yip <jake.yip@unimelb.edu.au>
2024-05-15 15:52:18 +1000

    Add class to config capi-helm driver
    This is a squash of the following Changes:
    - I0e9409628c2fa70557dc4a10db826e8425057f01
    - I709c3e198d562c9cabfe04f408330d35eb91d658
    - Icbac64d3633508e13007269f40c633344aa798de
    
    Change-Id: Ie813860822e173521a5c62f1c419e6602579b3b1
    (cherry picked from commit e1f4d364e5cda4ba074102da875952fab4384ae7)

---
+++ manifests/driver/capi_helm.pp
@@ -0,0 +1,41 @@
+# == Class: magnum::driver::capi_helm
+#
+# Configures the Magnum capi_helm driver
+#
+# === Parameters
+#
+# [*kubeconfig_file*]
+#   Path to a kubeconfig file for a management cluster
+#   Defaults to $::os_service_default
+#
+# [*helm_chart_repo*]
+#   Reference to the helm chart repository for the cluster API driver.
+#   Defaults to $::os_service_default
+#
+# [*helm_chart_name*]
+#   Name of the helm chart to use from the repo specified by the helm_chart_repo.
+#   Defaults to $::os_service_default
+#
+# [*csi_cinder_default_volume_type*]
+#   Default StorageClass volume type for persistent volumes.
+#   Defaults to $::os_service_default
+#
+# [*csi_cinder_reclaim_policy*]
+#   Policy for reclaiming dynamically created persistent volumes.
+#   Defaults to $::os_service_default
+#
+class magnum::driver::capi_helm (
+  $kubeconfig_file                = $facts['os_service_default'],
+  $helm_chart_repo                = $facts['os_service_default'],
+  $helm_chart_name                = $facts['os_service_default'],
+  $csi_cinder_default_volume_type = $facts['os_service_default'],
+  $csi_cinder_reclaim_policy      = $facts['os_service_default'],
+) {
+  magnum_config {
+    'capi_helm/kubeconfig_file': value => $kubeconfig_file;
+    'capi_helm/helm_chart_repo': value => $helm_chart_repo;
+    'capi_helm/helm_chart_name': value => $helm_chart_name;
+    'capi_helm/csi_cinder_default_volume_type': value => $csi_cinder_default_volume_type;
+    'capi_helm/csi_cinder_reclaim_policy': value => $csi_cinder_reclaim_policy;
+  }
+}
