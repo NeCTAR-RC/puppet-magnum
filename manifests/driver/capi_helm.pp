67e15b4488e79e5a2793abb75a7ef52b6c0b7fab
Jake Yip <jake.yip@ardc.edu.au>
2024-07-11 18:02:14 +1000

    Add additional config for capi_helm driver
    Change-Id: I709c3e198d562c9cabfe04f408330d35eb91d658

--- manifests/driver/capi_helm.pp
+++ manifests/driver/capi_helm.pp
@@ -8,12 +8,24 @@
 #   Path to a kubeconfig file for a management cluster
 #   Defaults to $::os_service_default
 #
+# [*helm_chart_repo*]
+#   Reference to the helm chart repository for the cluster API driver.
+#   Defaults to $::os_service_default
+#
+# [*helm_chart_name*]
+#   Name of the helm chart to use from the repo specified by the helm_chart_repo.
+#   Defaults to $::os_service_default
+#
 class magnum::driver::capi_helm (
   $kubeconfig_file  = $::os_service_default,
+  $helm_chart_repo  = $::os_service_default,
+  $helm_chart_name  = $::os_service_default,
 ) {
 
   magnum_config {
-    'capi_helm/kubeconfig_file': value => $kubeconfig_file,
+    'capi_helm/kubeconfig_file': value => $kubeconfig_file;
+    'capi_helm/helm_chart_repo': value => $helm_chart_repo;
+    'capi_helm/helm_chart_name': value => $helm_chart_name;
   }
 
 }
