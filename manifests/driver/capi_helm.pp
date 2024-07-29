8b65caa5adf5da37bac56e1892aecdb58aa8af4f
Jake Yip <jake.yip@ardc.edu.au>
2024-07-29 12:32:20 +1000

    Add csi-cinder config for capi-helm driver
    Change-Id: Icbac64d3633508e13007269f40c633344aa798de
    (cherry picked from commit b29426627310c121a432a51fa7c780c6c62397a7)

--- manifests/driver/capi_helm.pp
+++ manifests/driver/capi_helm.pp
@@ -16,16 +16,28 @@
 #   Name of the helm chart to use from the repo specified by the helm_chart_repo.
 #   Defaults to $::os_service_default
 #
+# [*csi_cinder_default_volume_type*]
+#   Default StorageClass volume type for persistent volumes.
+#   Defaults to $::os_service_default
+#
+# [*capi_helm/csi_cinder_reclaim_policy*]
+#   Policy for reclaiming dynamically created persistent volumes.
+#   Defaults to $::os_service_default
+#
 class magnum::driver::capi_helm (
-  $kubeconfig_file  = $::os_service_default,
-  $helm_chart_repo  = $::os_service_default,
-  $helm_chart_name  = $::os_service_default,
+  $kubeconfig_file                = $::os_service_default,
+  $helm_chart_repo                = $::os_service_default,
+  $helm_chart_name                = $::os_service_default,
+  $csi_cinder_default_volume_type = $::os_service_default,
+  $csi_cinder_reclaim_policy      = $::os_service_default,
 ) {
 
   magnum_config {
-    'capi_helm/kubeconfig_file': value => $kubeconfig_file;
-    'capi_helm/helm_chart_repo': value => $helm_chart_repo;
-    'capi_helm/helm_chart_name': value => $helm_chart_name;
+    'capi_helm/kubeconfig_file': value                => $kubeconfig_file;
+    'capi_helm/helm_chart_repo': value                => $helm_chart_repo;
+    'capi_helm/helm_chart_name': value                => $helm_chart_name;
+    'capi_helm/csi_cinder_default_volume_type': value => $csi_cinder_default_volume_type;
+    'capi_helm/csi_cinder_reclaim_policy': value      => $csi_cinder_reclaim_policy;
   }
 
 }
